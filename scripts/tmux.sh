b7efbc9f24ac8a401d7bccd507af1d75b3bfa62d
Stelin Larios <stelinlarios@gmail.com>
2025-10-02 18:28:54 -0600

    uwu

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -4,6 +4,7 @@ source "$REPO_DIR/scripts/lib/pkg.sh"
 
 echo "n\ ...Installing tmux..."
 pkg_install tmux
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 
 rm -rf "$HOME/.config/tmux"
 ln -s "$REPO_DIR/.config/tmux" "$HOME/.config/tmux"
