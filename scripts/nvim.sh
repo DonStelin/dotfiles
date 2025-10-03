808f77155a74c981072c06a74c59b862ba5a77c8
Stelin Larios <stelinlarios@gmail.com>
2025-10-02 18:14:28 -0600

    oopsieeee

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -2,7 +2,7 @@
 source "$REPO_DIR/scripts/lib/pkg.sh"
 
 echo "n\ ...Installing neovim..."
-pkg_install nvim
+pkg_install neovim
 
 rm -rf "$HOME/.config/nvim"
 ln -s "$REPO_DIR/.config/nvim"  "$HOME/.config/nvim"
