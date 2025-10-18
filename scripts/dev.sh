04d9e08033cbbaf2db12c93a847261cfd2c58a80
Stelin Larios <stelinlarios@gmail.com>
2025-10-18 06:58:52 -0600

    Autocommit

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 source "$REPO_DIR/scripts/lib/pkg.sh"
 
+
+echo "...::Directories::..."
+mkdir -p "$HOME/College" "$HOME/Dev" "$HOME/Notes" "$HOME/Environment"
+
 echo "...::Installing dev utilities::..."
-pkg_install fzf go typst nvm ghostty
+pkg_install fzf go typst nvm ghostty yq
 
--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -4,7 +4,6 @@ hypr_conf_dir="$HOME/.config/hypr"
 hypr_conf_file="$hypr_conf_dir/hyprland.conf"
 repo_hypr_conf="$REPO_DIR/env/.config/hypr"
 
-
 if [[ -f "$hypr_conf_file" ]]
 then
     ln -sf "$repo_hypr_conf/custom.conf" "$hypr_conf_dir/"
