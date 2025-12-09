0ad7036ced289a3a24ed6c47ad27095c574d3240
Stelin Larios <stelinlarios@gmail.com>
2025-12-09 09:05:51 -0600

    Autocommit

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 source "$REPO_DIR/scripts/lib/pkg.sh"
 
-
 echo "...::Directories::..."
 mkdir -p "$HOME/College" "$HOME/Dev" "$HOME/Notes" "$HOME/Environment"
 
-echo "...::Installing dev utilities::..."
-pkg_install fzf go typst nvm ghostty yq
+echo "...::Installing yazi::..."
+pkg_install yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
 
+echo "...::Installing dev utilities::..."
+pkg_install fzf go typst nvm yq
