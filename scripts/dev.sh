04ff65f23a9aefc7527f00e65e5f869dfb8f8c8b
Stelin Larios <stelinlarios@gmail.com>
2025-12-09 09:34:42 -0600

    Autocommit

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+REPO_DIR="$HOME/Dotfiles/"
+yazi_path="$HOME/.config/yazi"
+
 source "$REPO_DIR/scripts/lib/pkg.sh"
 
 echo "...::Directories::..."
@@ -6,6 +9,10 @@ mkdir -p "$HOME/College" "$HOME/Dev" "$HOME/Notes" "$HOME/Environment"
 
 echo "...::Installing yazi::..."
 pkg_install yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
+
+ln -s "$REPO_DIR/env/.config/yazi" "$yazi_path"
+
+ya pkg add dedukun/bookmarks
 
 echo "...::Installing dev utilities::..."
 pkg_install fzf go typst nvm yq
