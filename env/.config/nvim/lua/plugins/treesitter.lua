2b1deb0a931a0f6b7d7d7628205e2030cc925d82
Stelin Larios <stelinlarios@gmail.com>
2025-12-14 20:32:26 -0600

    Autocommit

--- env/.config/nvim/lua/plugins/treesitter.lua
+++ env/.config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,34 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require 'nvim-treesitter.configs'.setup {
-        ensure_installed = {
-          'bash',
-          'c',
-          'html',
-          'lua',
-          'markdown',
-          'vim',
-          'vimdoc',
-          'go',
-          'gomod',
-          'gosum',
-          'gowork',
-          'javascript',
-          'typescript',
-          'tsx',
-          'jsdoc',
-          'typst'
-        },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-      }
-    end,
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash",
+					"c",
+					"html",
+					"lua",
+					"markdown",
+					"vim",
+					"vimdoc",
+					"go",
+					"gomod",
+					"gosum",
+					"gowork",
+					"javascript",
+					"typescript",
+					"tsx",
+					"jsdoc",
+					"typst",
+					"latex",
+				},
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
 }
