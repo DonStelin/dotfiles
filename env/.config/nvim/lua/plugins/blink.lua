6f5434efc0d961f36e4924e96cb9f223c0e38f22
Stelin Larios <stelinlarios@gmail.com>
2025-12-16 22:04:00 -0600

    Autocommit

--- env/.config/nvim/lua/plugins/blink.lua
+++ env/.config/nvim/lua/plugins/blink.lua
@@ -39,7 +39,7 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "snippets", "path" },
+			default = { "snippets", "lsp", "path" },
 		},
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
