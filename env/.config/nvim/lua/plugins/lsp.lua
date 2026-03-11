e4d6a396cf358e1716019bcb51c6ae213222fd19
Stelin Larios <stelinlarios@gmail.com>
2026-03-10 19:17:52 -0600

    Autocommit

--- env/.config/nvim/lua/plugins/lsp.lua
+++ env/.config/nvim/lua/plugins/lsp.lua
@@ -26,37 +26,18 @@ return {
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-					map("<leader>ca", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-					map("gd", require("snacks").picker.lsp_definitions, "[G]oto [D]definition")
+					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+					map("gd", require("snacks").picker.lsp_definitions, "[G]oto [D]efinition")
 					map("grr", require("snacks").picker.lsp_references, "[G]oto [R]eferences")
 					map("gI", require("snacks").picker.lsp_implementations, "[G]oto [I]mplementation")
-					map("<leader>fS", require("snacks").picker.lsp_symbols, "[D]ocument [S]symbols")
+					map("grt", require("snacks").picker.lsp_type_definitions, "[G]oto [T]ype Definition")
+					map("<leader>fS", require("snacks").picker.lsp_symbols, "[D]ocument [S]ymbols")
+					map("<leader>ws", require("snacks").picker.lsp_workspace_symbols, "[W]orkspace [S]ymbols")
 					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-					map("<leader>ws", require("snacks").picker.lsp_workspace_symbols, "[W]orkspace [S]symbols")
-
-					---@param client vim.lsp.Client
-					---@param method vim.lsp.protocol.Method
-					---@param bufnr? integer some lsp support methods only in specific files
-					---@return boolean
-					local function client_supports_method(client, method, bufnr)
-						if vim.fn.has("nvim-0.11") == 1 then
-							return client:supports_method(method, bufnr)
-						else
-							return client.supports_method(method, { bufnr = bufnr })
-						end
-					end
-
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if
-						client
-						and client_supports_method(
-							client,
-							vim.lsp.protocol.Methods.textDocument_documentHighlight,
-							event.buf
-						)
-					then
+					if client and client:supports_method("textDocument/documentHighlight", event.buf) then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -80,10 +61,7 @@ return {
 						})
 					end
 
-					if
-						client
-						and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
-					then
+					if client and client:supports_method("textDocument/inlayHint", event.buf) then
 						map("<leader>th", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 						end, "[T]oggle Inlay [H]ints")
@@ -92,6 +70,7 @@ return {
 			})
 
 			vim.diagnostic.config({
+				update_in_insert = false,
 				severity_sort = true,
 				float = { border = "rounded", source = "if_many" },
 				underline = { severity = vim.diagnostic.severity.ERROR },
@@ -106,20 +85,12 @@ return {
 				virtual_text = {
 					source = "if_many",
 					spacing = 2,
-					format = function(diagnostic)
-						local diagnostic_message = {
-							[vim.diagnostic.severity.ERROR] = diagnostic.message,
-							[vim.diagnostic.severity.WARN] = diagnostic.message,
-							[vim.diagnostic.severity.INFO] = diagnostic.message,
-							[vim.diagnostic.severity.HINT] = diagnostic.message,
-						}
-						return diagnostic_message[diagnostic.severity]
-					end,
 				},
+				virtual_lines = false,
+				jump = { float = true },
 			})
 
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
-
+			---@type table<string, vim.lsp.Config>
 			local servers = {
 				bashls = {},
 				clangd = {},
@@ -132,7 +103,16 @@ return {
 				svelte = {},
 				tailwindcss = {},
 				tinymist = {},
-				ts_ls = {},
+				ts_ls = {
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"javascript.jsx",
+						"typescript",
+						"typescriptreact",
+						"typescript.tsx",
+					},
+				},
 				lua_ls = {
 					settings = {
 						Lua = {
@@ -143,6 +123,7 @@ return {
 					},
 				},
 			}
+
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua",
@@ -154,17 +135,10 @@ return {
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-			require("mason-lspconfig").setup({
-				ensure_installed = {},
-				automatic_installation = false,
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
+			for name, config in pairs(servers) do
+				vim.lsp.config(name, config)
+				vim.lsp.enable(name)
+			end
 		end,
 	},
 }
