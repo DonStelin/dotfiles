14fb2c6bac96d4c438730c40829a3bc8815af43b
Stelin Larios <stelinlarios@gmail.com>
2026-05-14 15:53:57 -0600

    Autocommit

--- env/.config/nvim-0.12/init.lua
+++ env/.config/nvim-0.12/init.lua
@@ -28,7 +28,6 @@ require("oil").setup({
   keymaps = {
     ["g?"] = { "actions.show_help", mode = "n" },
     ["<CR>"] = "actions.select",
-    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
     ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
     ["<C-t>"] = { "actions.select", opts = { tab = true } },
     ["<C-p>"] = "actions.preview",
